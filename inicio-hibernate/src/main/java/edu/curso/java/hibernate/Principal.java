c6209c57dc8704b74fe25ccf8eaac91f7ca7fb41
Claudio Zamoszczyk <claudio.zamoszczyk@gmail.com>
2023-07-26 11:49:54 -0300

    ejemplo de alta / consulta / borrado con hibernate

--- inicio-hibernate/src/main/java/edu/curso/java/hibernate/Principal.java
+++ inicio-hibernate/src/main/java/edu/curso/java/hibernate/Principal.java
@@ -16,9 +16,23 @@ public class Principal {
 		producto1.setDescripcion("Ejemplo de producto con hibernate");
 		producto1.setPrecio(5000.0);
 		
-		session.save(producto1); //GRABA EL OBJETO PRODUCTO EN LA BASE DE DATOS!!!!!
-		
+		//******* ALTA DE PRODUCTO
+		session.save(producto1); //GRABA EL OBJETO PRODUCTO EN LA BASE DE DATOS!!!!!		
 		System.out.println("ID Autogenerado por hibernate: " + producto1.getId());
+		
+		
+		//******* CONSULTA DE PRODUCTO POR ID
+		//Long idBuscar = 60L;
+		//Producto productoBuscar = session.load(Producto.class, idBuscar);
+		//System.out.println("Nombre: " + productoBuscar.getNombre() + "Descripcion: " + productoBuscar.getDescripcion());
+		
+		//******* ACTUALIZAR PRODUCTO
+		//productoBuscar.setDescripcion("Nueva descripcion......");
+		//session.update(productoBuscar);
+		
+		//*******  BORRADO DEL PRODUCTO
+		//session.delete(productoBuscar);
+		
 		
 		transaction.commit(); // CONFIRMA LOS CAMBIOS EN LA BASE DE DATOS
 		// transaction.rollback(); // PUEDO BORRAR LOS CAMBIOS SI NO LOS QUIERO!!!!!
