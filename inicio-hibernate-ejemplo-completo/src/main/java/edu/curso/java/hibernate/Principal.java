beb96c6db6f0a3322107da6f441865c415a26450
Claudio Zamoszczyk <claudio.zamoszczyk@gmail.com>
2023-08-02 10:32:34 -0300

    cambios para dar de alta y mostrar un reclamo

--- inicio-hibernate-ejemplo-completo/src/main/java/edu/curso/java/hibernate/Principal.java
+++ inicio-hibernate-ejemplo-completo/src/main/java/edu/curso/java/hibernate/Principal.java
@@ -1,5 +1,6 @@
 package edu.curso.java.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.*;
@@ -14,9 +15,55 @@ public class Principal {
 		Transaction transaction = session.beginTransaction(); //INIO DE LA TRANSACTION
 		
 		//EJEMPLO DE ALTA DE ESTADO DE RECLAMO
-		EstadoReclamo estadoReclamo = new EstadoReclamo();
-		estadoReclamo.setNombre("Resuelto");
-		session.save(estadoReclamo);
+		//EstadoReclamo estadoReclamo = new EstadoReclamo();
+		//estadoReclamo.setNombre("Resuelto");
+		//session.save(estadoReclamo);
+		//****************************************************
+		
+		//ALTA DE CLIENTES
+		//Cliente cliente1 = new Cliente();
+		//cliente1.setNombre("Juan Perez");
+		//session.save(cliente1);
+		
+		//Cliente cliente2 = new Cliente();
+		//cliente2.setNombre("Maria Lopez");
+		//session.save(cliente2);
+		//****************************************************
+		
+		
+		//EJEMPLO DE ALTA DE RECLAMO
+//		Long idEstadoReclamoNuevo = 1L;
+//		EstadoReclamo estadoReclamoNuevo = session.load(EstadoReclamo.class, idEstadoReclamoNuevo);
+//		
+//		Long idClienteJuanPerez = 5L;
+//		Cliente clienteJuanPerez = session.load(Cliente.class, idClienteJuanPerez);
+//		
+//		Reclamo reclamo = new Reclamo();
+//		reclamo.setEstadoReclamo(estadoReclamoNuevo);
+//		reclamo.setFechaDeAlta(new Date()); //TOMA LA FECHA Y HORA ACTUAL DE LA PC!!!!
+//		reclamo.setTitulo("Ejemplo de reclamo 1");
+//		reclamo.setDescripcion("Este es un ejemplo mas largo de texto para el reclamo");
+//		session.save(reclamo);
+//		
+//		clienteJuanPerez.getReclamos().add(reclamo); //ESTOY AGREGANDO UN RECLAMO AL CLIENTE
+//		session.update(clienteJuanPerez);
+//		
+		//****************************************************
+
+		
+		Long idReclamoBuscar = 7L;
+		Reclamo reclamo = session.load(Reclamo.class, idReclamoBuscar);
+
+		System.out.println("Id: " + reclamo.getId());
+		System.out.println("Titulo: " + reclamo.getTitulo());
+		System.out.println("Descripcion: " + reclamo.getDescripcion());
+		System.out.println("Estado del reclamo " + reclamo.getEstadoReclamo().getNombre());
+		System.out.println("Fecha alta: " + reclamo.getFechaDeAlta());
+		
+		
+		
+		//****************************************************
+
 		
 		transaction.commit(); 
 		session.close(); 
