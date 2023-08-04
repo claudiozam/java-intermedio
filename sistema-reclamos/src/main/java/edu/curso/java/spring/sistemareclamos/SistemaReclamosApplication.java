7d83a6434e1fffbccb55a789c70f62382a7ffde4
Claudio Zamoszczyk <claudio.zamoszczyk@gmail.com>
2023-08-04 11:54:10 -0300

    primeros pasos jpa

--- sistema-reclamos/src/main/java/edu/curso/java/spring/sistemareclamos/SistemaReclamosApplication.java
+++ sistema-reclamos/src/main/java/edu/curso/java/spring/sistemareclamos/SistemaReclamosApplication.java
@@ -3,11 +3,13 @@ package edu.curso.java.spring.sistemareclamos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import edu.curso.java.spring.sistemareclamos.repository.ClientesRepository;
+
 @SpringBootApplication
 public class SistemaReclamosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaReclamosApplication.class, args);
+		SpringApplication.run(SistemaReclamosApplication.class, args);	
 	}
 
 }
